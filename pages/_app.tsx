c85e8447a9a1f19c2ea68a208ad78bc3e2d6f7c5
tahawy111 <amer.vib@gmail.com>
2023-02-05 23:16:09 +0200

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Sidebar from "@/components/Sidebar";
+import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      <Sidebar>
+        <Component {...pageProps} />
+      </Sidebar>
+    </ThemeProvider>
+  );
 }
